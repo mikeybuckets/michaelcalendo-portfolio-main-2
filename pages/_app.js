741e708573f538626e3ab46e8f4d5c2d183c4d90
mikeybuckets <mikeycalendo20@gmail.com>
2021-08-18 14:11:31 -0700

    8182021 deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
 import Router from  "next/router";
-import { Helmet } from 'react-helmet';
 
 import ProgressBar from "@badrap/bar-of-progress";
 
@@ -16,18 +15,6 @@ Router.events.on("routeChangeStart", progress.start);
 Router.events.on("routeChangeComplete", progress.finish);
 Router.events.on("routeChangeError", progress.finish);
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
-}
-
-export default function App() {
-  return (
-    <div className="App">
-      <Helmet>
-        <title>App Title</title>
-        <meta name="description" content="App Description" />
-        <meta name="theme-color" content="#008f68" />
-      </Helmet>
-    </div>
-  );
 }
