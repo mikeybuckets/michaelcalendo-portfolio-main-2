17330a589850ff21596bda8e870f282ba1bad67b
mikeybuckets <mikeycalendo20@gmail.com>
2021-08-18 14:01:19 -0700

    fixed

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from "next/Head";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
